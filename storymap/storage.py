1d7a38284be7bb08a6dbd060121f4a5b17c9271e
Alexander Hammatt <alexander.hammatt@ecs.vuw.ac.nz>
2016-09-01 16:13:50 +1200

    Some work towards convering from AWS to usinga  local minio server, currently wont connect.

--- storymap/storage.py
+++ storymap/storage.py
@@ -10,7 +10,9 @@ import time
 import traceback
 import json
 from functools import wraps
-import boto
+import boto3
+import botocore
+from botocore.client import ClientError
 from moto import mock_s3
 from boto.exception import S3ResponseError
 from boto.s3.connection import OrdinaryCallingFormat
@@ -19,12 +21,13 @@ import requests
 # Get settings module
 settings = sys.modules[os.environ['FLASK_SETTINGS_MODULE']]
 
+
 if settings.TEST_MODE:
     _mock = mock_s3()
     _mock.start()
 
-    _conn = boto.connect_s3()
-    _bucket = _conn.create_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+    _conn = boto3.resource('s3')
+    _bucket = _conn.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
     _mock.stop()
 else:
@@ -56,14 +59,14 @@ def _mock_in_test_mode(f):
 
 
 def _reraise_s3response(f):
-    """Decorator trap and re-raise S3ResponseError as StorageException"""
+    """Decorator trap and re-raise S3ResponseError (ClientError) as StorageException"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         try:
             return f(*args, **kwargs)
-        except S3ResponseError, e:
+        except ClientError, e:
             print traceback.format_exc()
-            raise StorageException(e.message, e.body)
+            raise StorageException(e, "")
     return decorated_function
 
 
@@ -91,7 +94,7 @@ def list_keys(key_prefix, n, marker=''):
     key_list = []
     i = 0
 
-    for i, item in enumerate(_bucket.list(prefix=key_prefix, marker=marker)):
+    for i, item in enumerate(_bucket.objects.all()):
         if i == n:
             break
         if item.name == key_prefix:
@@ -105,7 +108,7 @@ def get_contents_as_string(src_key):
 
 @_mock_in_test_mode
 def all_keys():
-    for item in _bucket.list(prefix=settings.AWS_STORAGE_BUCKET_KEY):
+    for item in enumerate(_bucket.objects.all()):
         if item.name == key_prefix:
             continue
         yield item.key
@@ -122,7 +125,7 @@ def list_key_names(key_prefix, n, marker=''):
     name_list = []
     i = 0
 
-    for i, item in enumerate(_bucket.list(prefix=key_prefix, marker=marker)):
+    for i, item in enumerate(_bucket.objects.all()):
         if i == n:
             break
         if item.name == key_prefix:
